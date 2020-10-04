192cb6b9943ae445dbdd6a11de9a46663f265409
Alexandra-Lischt <49699293+Alexandra-Lischt@users.noreply.github.com>
2020-10-04 17:21:29 -0300

    initial

---
+++ front-end/src/app/directives/for.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, OnInit, Input, ViewContainerRef, TemplateRef } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+
+  constructor(
+
+    ) {}
+
+  ngOnInit(): void {
+
+  }
+
+}
